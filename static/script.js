5c35bde7fbb7e3a30d5e9790d74407c2193e6223
manivenkatasai <medammani4@gmail.com>
2026-01-15 21:56:33 +0530

    aaaa

--- static/script.js
+++ static/script.js
@@ -8,7 +8,7 @@ async function send() {
     chat.innerHTML += `<div class="user">You (${role}): ${text}</div>`;
     document.getElementById("input").value = "";
 
-    const res = await fetch("http://127.0.0.1:5000/ask", {
+    const res = await fetch("/ask", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question: text, role })
